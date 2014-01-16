acc198590f8b78e3f8a1f2dcfca4504c321b7eff
lbuell <liamcbuell@gmail.com>
2014-01-15 19:40:37 -0800

    Updated Readme

---
+++ README.rdoc
@@ -0,0 +1,17 @@
+# Basic Rails App Using Foundation
+<!-- If you'd like to use a logo instead uncomment this code and remove the text above this line
+
+  ![Logo](URL to logo img file goes here)
+
+-->
+
+By [Liam Buell](http://www.liambuell.com).
+
+## Description
+**Rails App** This is a very basic but functional product app using Foundation
+
+
+### Known Issues
+
+This is a work in progress. Your comments and contributions are welcome! Please open an issue if you run in to any trouble.
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = 'e0dbdcdbc57c6adaeb3ca820af4a25faec946328e8c276f110ebe1777d001dd479a31be53324fd9b6da209e25624f1d48fdb88d6c39ebe122dba8fcbb4b0acc2'
